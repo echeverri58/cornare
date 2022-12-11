c5f5df2ccd79a632cca80ce7f6f02cbcd57709e5
echeverri58 <85091279+echeverri58@users.noreply.github.com>
2022-12-10 20:13:39 -0500

    Add files via upload

---
+++ resaltapdf.py
@@ -0,0 +1,19 @@
+# Crea una lista de palabras a buscar
+palabras = ["San Luis", "energía", "pch", "PCH", "hidroeléctrica"]
+
+os.mkdir('separados')
+path= r"/content/PDF/"
+path2 = r"/content/separados"
+files = os.listdir(path)
+
+for file in files:
+    doc=fitz.open(path+'/'+file)
+    for page in doc:
+        text = page.get_text()
+        # Busca cada palabra en la lista en el texto de la página
+        for palabra in palabras:
+            result = text.find(palabra)
+            if result != -1:
+                doc.save(path2+'/'+file)
+                print(file)
+                pass
